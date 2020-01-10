e309e02ad1c633bcbd446c6837f05db05b3bab9c
pikaChin <derrfgh4563@gmail.com>
2020-01-10 10:48:47 +0800

    Merge with develop

--- app/src/main/java/com/example/pikacount/viewStructure/PageView.java
+++ app/src/main/java/com/example/pikacount/viewStructure/PageView.java
@@ -1,7 +1,6 @@
 package com.example.pikacount.viewStructure;
 
 import android.content.Context;
-<<<<<<< HEAD
 import android.widget.RelativeLayout;
 
 public abstract class PageView extends RelativeLayout {
@@ -10,14 +9,4 @@ public abstract class PageView extends RelativeLayout {
     }
 
     public void updateList() {}
-=======
-
-import androidx.constraintlayout.widget.ConstraintLayout;
-
-public abstract class PageView extends ConstraintLayout {
-
-    public PageView(Context context) {
-        super(context);
-    }
->>>>>>> 8ef4810534be616c5feb1ef06348630bb242dfa2
 }
